b3b9bdb5b62fc0a63c9cdcbbfb17902f3d0b0a77
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-05-22 23:19:45 +0100

    Started MyEnumerable

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T>
+    {
+        private T[] items = new T[3];
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public void SetItem(int index, T value)
+        {
+            items[index] = value;
+        }
+
+    }
+
+}
