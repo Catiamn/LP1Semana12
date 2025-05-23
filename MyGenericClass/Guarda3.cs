90d87b3d6b4e233f653af3e066a196c8ea47c2ec
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-05-23 12:31:41 +0100

    Changed MyGenericClass

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -8,11 +8,13 @@ namespace MyGenericClass
 
     public T GetItem(int index)
     {
+         if (index < 0 || index > 2) throw new IndexOutOfRangeException();
         return items[index];
     }
 
     public void SetItem(int index, T value)
     {
+         if (index < 0 || index > 2) throw new IndexOutOfRangeException();
         items[index] = value;
     }
 }
