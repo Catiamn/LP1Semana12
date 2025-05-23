fdd8e382f8a3102de246216fb6e6f451802644ea
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-05-23 12:49:09 +0100

    Added MyEnumerable

--- MyEnumerable/Guarda3.cs
+++ MyEnumerable/Guarda3.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace MyEnumerable
 {
@@ -8,12 +9,22 @@ namespace MyEnumerable
 
         public T GetItem(int index)
         {
+            if (index < 0 || index > 2) throw new IndexOutOfRangeException();
             return items[index];
         }
 
         public void SetItem(int index, T value)
         {
+            if (index < 0 || index > 2) throw new IndexOutOfRangeException();
             items[index] = value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                yield return items[i];
+            }
         }
 
     }
